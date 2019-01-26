4155ad72f3b2971e343af53fcd1805183a80063e
Romiss <Romissevd@gmail.com>
2019-01-27 00:00:40 +0200

    user signin #1

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('sign_in/', views.sign_in, name='sign_in'),
     path('registration/', views.registration, name='registration')
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from user.form import LoginForm
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 # def index(request):
@@ -10,6 +11,22 @@ from django.http import HttpResponse
 def login(request):
 
     return render(request, 'login.html', {'form': LoginForm})
+
+def sign_in(request):
+    errors = []
+    if request.method == 'POST':
+        # print(request.POST.keys())
+        if not request.POST.get('email', ''):
+            errors.append('Введите email')
+        if not request.POST.get('password', ''):
+            errors.append('Введите пароль')
+        if errors:
+            print(errors)
+            return render(request, 'login.html', {
+                'form': LoginForm,
+                'errors': errors,
+                })
+        return HttpResponseRedirect('/')
 
 def registration(request):
 
