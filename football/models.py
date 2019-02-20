c70b4fe3b52c61371b6f47e8579db21bd4216aea
Romiss <Romissevd@gmail.com>
2019-02-20 16:04:17 +0200

    refactor parser data about football clubs.

--- football/models.py
+++ football/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class FCStadium(models.Model):
+
+    name = models.CharField(max_length=200, default='')
+    city = models.CharField(max_length=200, default='')
+    capacity = models.IntegerField(default=0)
+
+
+class FootballClub(models.Model):
+
+    eng_name = models.CharField(max_length=200)
+    ru_name = models.CharField(max_length=200)
+    id_image = models.IntegerField(default=0)
+    year_of_foundation = models.DateField(null=True)
+    address = models.CharField(max_length=200, default='')
+    country = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200, default='')
+    fax = models.CharField(max_length=200, default='')
+    email = models.EmailField(default='')
+    stadium = models.ForeignKey(FCStadium, on_delete=models.CASCADE)
--- parser/parsing_football.py
+++ parser/parsing_football.py
@@ -18,50 +18,56 @@ def path_to_driver_browser(webDriver):
 
     return path
 
+
 def run_webdriver(nameDriver):
     path = path_to_driver_browser(nameDriver)
 
     return webdriver.Chrome(executable_path=path)
 
+
+def search_list_info(path, page):
+    bsObj = BeautifulSoup(page, 'html.parser')
+    country_list = bsObj.select(path)
+    return country_list
+
+
+
 def open_site(url):
     driver = run_webdriver(WEBDRIVER)
     driver.get(url)
-    time.sleep(10)
+    time.sleep(5)
     page = driver.page_source
-    bsObj = BeautifulSoup(page, 'html.parser')
-
-    pos = 1
-    for info in bsObj.select('#page_teams_1_block_teams_index_club_teams_2 > ul > li'):
-
-        print(info.text.strip())
-
-        driver.find_element_by_xpath('//li[@data-area_id="{}"]'.format(info["data-area_id"])).click()
+    number_row = 1
+    path_country_list = '#page_teams_1_block_teams_index_club_teams_2 > ul > li'
+    for country_info in search_list_info(path_country_list, page):
+        rus_name_country = country_info.text.strip()
+        print(country_info.text.strip())
+        driver.find_element_by_xpath('//li[@data-area_id="{}"]'.format(country_info["data-area_id"])).click()
         time.sleep(10)
-        page_1 = driver.page_source
-        bsObj_1 = BeautifulSoup(page_1, 'html.parser')
-
         num = 1
-
-
-        for infor in bsObj_1.select('#page_teams_1_block_teams_index_club_teams_2 > ul > li.expandable.loaded.expanded:nth-child({}) > ul > li'.format(pos)):
-            try :
-                driver.find_element_by_xpath('//*[@id="page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul[@class="competitions"]'.format(pos))
-
+        path_leagues = '#page_teams_1_block_teams_index_club_teams_2 > ul > li.expandable.loaded.expanded:nth-child({}) > ul > li'.format(number_row)
+        for leagues in search_list_info(path_leagues, driver.page_source):
+            try:
+                # in not leagues
+                path_search_leagues = '//*[@id="page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul[@class="competitions"]'.format(number_row)
+                driver.find_element_by_xpath(path_search_leagues)
             except:
-                i = driver.find_element_by_xpath(
+                lst_clubs = driver.find_element_by_xpath(
                     '//*[@id="page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul[@class="teams"]'.format(
-                        pos))
-                for inf in i.find_elements_by_tag_name("a"):
-                    print("-" * 8, inf.text, '==', inf.get_attribute('href'))
+                        number_row))
+                for club in lst_clubs.find_elements_by_tag_name("a"):
+                    print("-" * 8, club.text, '==', club.get_attribute('href'))
                 break
 
-            print("---- ", infor.text)
+            league_name = leagues.text
+
+            print("---- ", league_name)
             if num % 2 == 1 and driver.find_element_by_xpath('//li[@class="expandable odd"]'):
-                driver.find_element_by_xpath('//*[@id = "page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul/li[@class="expandable odd"]'.format(pos)).click()
+                driver.find_element_by_xpath('//*[@id = "page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul/li[@class="expandable odd"]'.format(number_row)).click()
                 time.sleep(3)
                 for link in driver.find_elements_by_xpath(
                     '//*[@id = "page_teams_1_block_teams_index_club_teams_2"]/ul/li[@class = "expandable  expanded loaded"][{}]/ul/li[{}]/ul/li'.format(
-                        pos, num)):
+                        number_row, num)):
 
 
                     print("-"*8, link.text, "==",link.find_element_by_tag_name("a").get_attribute('href'))
@@ -69,23 +75,23 @@ def open_site(url):
             elif num % 2 == 0 and driver.find_element_by_xpath('//li[@class="expandable even"]'):
                 driver.find_element_by_xpath(
                     '//*[@id = "page_teams_1_block_teams_index_club_teams_2"]/ul/li[{}]/ul/li[@class="expandable even"]'.format(
-                        pos)).click()
+                        number_row)).click()
 
                 time.sleep(3)
                 for link in driver.find_elements_by_xpath(
                     '//*[@id = "page_teams_1_block_teams_index_club_teams_2"]/ul/li[@class = "expandable  expanded loaded"][{}]/ul/li[{}]/ul/li'.format(
-                    pos, num)):
+                    number_row, num)):
                     print("-"*8, link.text, '==', link.find_element_by_tag_name("a").get_attribute('href'))
 
             else:
-                print("-" * 8, link.text, '==', link.find_element_by_tag_name("a").get_attribute('href'))
+                break
 
             num += 1
 
             time.sleep(5)
             if num == 4:
                 break
-        pos += 1
+        number_row += 1
     driver.close()
 
 
