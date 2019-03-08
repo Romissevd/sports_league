2433a5378959d9d301993803cc9587f4b657b324
Romiss <Romissevd@gmail.com>
2019-03-09 00:21:56 +0200

    api champions league #4

--- football/views.py
+++ football/views.py
@@ -10,25 +10,32 @@ def team_game(name):
         return {'team': team, 'image': img}
     except FootballClub.MultipleObjectsReturned:
         team = FootballClub.objects.filter(fc_en_name=name)
-        img = "/fc_logo/{}.png".format(team[0].num_image)
-        return {'team': team[0], 'image': img}
+        club = team[0]
+        for t in team:
+            if club.num_image > t.num_image:
+                club = t
+        img = "/fc_logo/{}.png".format(club.num_image)
+        return {'team': club, 'image': img}
     except FootballClub.DoesNotExist:
         names = ' '.join([x for x in name.split(' ') if len(x) > 2])
-        print('name = ',names)
         try:
             team = FootballClub.objects.get(fc_en_name__contains=names)
             img = "/fc_logo/{}.png".format(team.num_image)
             return {'team': team, 'image': img}
         except FootballClub.MultipleObjectsReturned:
             team = FootballClub.objects.filter(fc_en_name__contains=names)
-            img = "/fc_logo/{}.png".format(team[0].num_image)
-            return {'team': team[0], 'image': img}
+            club = team[0]
+            for t in team:
+                if club.num_image > t.num_image:
+                    club = t
+            img = "/fc_logo/{}.png".format(club.num_image)
+            return {'team': club, 'image': img}
         except FootballClub.DoesNotExist:
             return {'team': name}
 
 
 def champions_league(request):
-    print(FootballClub.objects.filter(fc_en_name='FK Dynamo Kyiv'))
+    # print(FootballClub.objects.filter(fc_en_name='FK Dynamo Kyiv'))
 
 
 # def champions_league(request):
@@ -41,6 +48,7 @@ def champions_league(request):
         m.update(stage=match['stage'])
         m.update(homeTeam=team_game(match['homeTeam']['name']))
         m.update(awayTeam=team_game(match['awayTeam']['name']))
+        m.update(score={'homeTeam': match['score']['fullTime']['homeTeam'], 'awayTeam': match['score']['fullTime']['awayTeam']})
         data['matches'].append(m)
     # print(data)
     # data = json.loads(data)
